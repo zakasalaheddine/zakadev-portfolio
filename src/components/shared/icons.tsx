b3d0bfcf1e5762fb0a788ed0b754e3b13fb3afa1
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2025-09-01 06:33:54 +0100

    feat: add grid svg to the layout

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -1,7 +1,29 @@
-import * as LucidIcons from "lucide-react"
+import * as LucidIcons from "lucide-react";
 
-export type Icon = React.ComponentType<LucidIcons.LucideIcon>
+export type Icon = React.SVGProps<SVGSVGElement>
 
 export const Icons = {
-  ...LucidIcons
-}
+	...LucidIcons,
+	BgGrid: (props: React.SVGProps<SVGSVGElement>) => (
+		<svg aria-hidden="true" {...props}>
+			<defs>
+				<pattern
+					id="hero"
+					width="80"
+					height="80"
+					x="50%"
+					y="-1"
+					patternUnits="userSpaceOnUse"
+				>
+					<path d="M.5 200V.5H200" fill="none" />
+				</pattern>
+			</defs>
+			<rect
+				width="100%"
+				height="100%"
+				stroke-width="0"
+				fill="url(#hero)"
+			/>
+		</svg>
+	),
+};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import DynamicIsland from "@/components/shared/dynamic-island";
+import { Icons } from "@/components/shared/icons";
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
@@ -39,6 +40,15 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 			<body>
 				<DynamicIsland />
 				<main className="pt-20">{children}</main>
+				<Icons.BgGrid
+					className="absolute inset-0 w-full h-[150vh] -z-10 dark:stroke-[rgba(255,250,245,0.06)] stroke-[rgba(0,0,0,0.1)] animate-[svgMask_1.5s_linear_forwards]"
+					style={{
+						WebkitMaskImage:
+							"radial-gradient(60% 800px at top center, var(--foreground), transparent)",
+						maskImage:
+							"radial-gradient(60% 800px at top center, var(--foreground), transparent)",
+					}}
+				/>
 				<TanstackDevtools
 					config={{
 						position: "bottom-left",
